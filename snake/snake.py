1ba85a9ea50a6a29259d03c66704e4a7c07ae4df
Admaster5678 <asmnum1@gmail.com>
2023-11-03 12:39:48 +0300

    Readability

--- snake/snake.py
+++ snake/snake.py
@@ -14,9 +14,12 @@ menu, game = True, True
 
 big_font = pygame.font.Font(path.realpath('assets')+'\\bit5x3.ttf', 100)
 title_text = big_font.render('PONG', False, white)
+tt_rect = ((858-title_text.get_width())//2, 525//3-title_text.get_height(), title_text.get_width(), title_text.get_height())
 
 medium_font = pygame.font.Font(path.realpath('assets')+'\\bit5x3.ttf', 25)
 button_text = medium_font.render('PLAY', False, white)
+bt_rect = ((858-button_text.get_width())//2, 525//2, button_text.get_width(), button_text.get_height())
+b_rect = ((858-bt_rect[2]*1.5)//2, 525//2-bt_rect[3]//3, bt_rect[2]*1.5, bt_rect[3]*1.5)
 
 while menu:
     for event in pygame.event.get():
@@ -29,14 +32,15 @@ while menu:
             elif event.key == pygame.K_RETURN: menu = False
 
         if event.type == pygame.MOUSEBUTTONUP:
-            if (858-button_text.get_width()*1.5)//2 <= event.pos[0] and event.pos[0] <= 858//2+button_text.get_width()*1.5//2: 
-                if 525//2-button_text.get_height()//3 <= event.pos[1] and event.pos[1] <= 525//2+button_text.get_height()*7//6:
+            if b_rect[0] <= event.pos[0] and event.pos[0] <= b_rect[0]+b_rect[2]:
+                if b_rect[1] <= event.pos[1] and event.pos[1] <= b_rect[1]+b_rect[3]:
                     menu = False
+
     
-    screen.blit(button_text, ((858-button_text.get_width())//2, 525//2))
-    pygame.draw.rect(screen, white, pygame.Rect((858-button_text.get_width()*1.5)//2, 525//2-button_text.get_height()//3,
-                                                button_text.get_width()*1.5, button_text.get_height()*1.5), 2)
-    screen.blit(title_text, ((858-title_text.get_width())//2,525//3-title_text.get_height()))
+
+    screen.blit(button_text, bt_rect[:2])
+    pygame.draw.rect(screen, white, pygame.Rect(b_rect), 2)
+    screen.blit(title_text, tt_rect[:2])
     pygame.display.update()
 
 
