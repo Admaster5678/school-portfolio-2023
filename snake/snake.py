a5a82f5bb6d22d75d85fb1b1bf02c2629b994f5d
Admaster5678 <asmnum1@gmail.com>
2023-11-12 20:10:37 +0300

    Fixed frame rate
    Allows for controllable speeds(paddles, ball)

--- snake/snake.py
+++ snake/snake.py
@@ -2,11 +2,12 @@ from pathlib import Path
 assets_dir = str(Path( __file__ ).parent.absolute()) + r'\assets'
 
 
-import pygame
+import pygame, time
 pygame.init()
 
 screen_size = (858, 525)
 screen = pygame.display.set_mode(screen_size)
+FPS = 60
 
 
 white = (255,)*3
@@ -22,6 +23,7 @@ button_text = medium_font.render('PLAY', False, white)
 bt_rect = ((screen_size[0]-button_text.get_width())//2, screen_size[1]//2, button_text.get_width(), button_text.get_height())
 b_rect = ((screen_size[0]-bt_rect[2]*1.5)//2, screen_size[1]//2-bt_rect[3]//3, bt_rect[2]*1.5, bt_rect[3]*1.5)
 
+prev_time = time.time()
 while menu:
     for event in pygame.event.get():
 
@@ -38,6 +40,11 @@ while menu:
                     menu = False
 
     
+    current_time = time.time()
+    dt = current_time-prev_time
+    prev_time = current_time
+    sleep_time = 1./FPS-dt
+    if sleep_time>0: time.sleep(sleep_time)
 
     screen.blit(button_text, bt_rect[:2])
     pygame.draw.rect(screen, white, pygame.Rect(b_rect), 2)
@@ -53,6 +60,7 @@ p2_pos = [screen_size[0]-paddle_size[0]-5,5]
 ball_pos = []
 ball_v = []
 
+prev_time = time.time()
 while game:
     for event in pygame.event.get():
 
@@ -60,7 +68,7 @@ while game:
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_ESCAPE: game = False
-        
+
     pressed_keys = pygame.key.get_pressed()
     if pressed_keys[pygame.K_w]:
         if p1_pos[1]-paddle_v>=5: p1_pos[1] -= paddle_v
@@ -71,7 +79,14 @@ while game:
     elif pressed_keys[pygame.K_DOWN]:
         if p2_pos[1]+paddle_v<=screen_size[1]-paddle_size[1]-5: p2_pos[1] += paddle_v
 
-    
+    current_time = time.time()
+    dt = current_time-prev_time
+    prev_time = current_time
+    sleep_time = 1./FPS-dt
+    if sleep_time>0:
+        print("slept for", sleep_time)
+        time.sleep(sleep_time)
+
     screen.fill((0,0,0))
     pygame.draw.ellipse(screen, white, p1_pos+paddle_size)
     pygame.draw.ellipse(screen, white, p2_pos+paddle_size)
