ba7cbcceaf869831b15429416e486a1ef8d849b1
Aditya Menon <asmnum1@gmail.com>
2023-11-18 21:21:37 +0300

    End screen

--- pong/pong.py
+++ pong/pong.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode(screen_size)
 FPS = 60
 
 white = (255,)*3
-menu, game = True, True
+menu, game, end_scr = True, True, True
 
 big_font = pygame.font.Font(str(Path(assets_dir, 'bit5x3.ttf')), 100)
 title_text = big_font.render('PONG', False, white)
@@ -28,10 +28,10 @@ while menu:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            game, menu = False, False
+            game, menu, end_scr = False, False, False
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_ESCAPE: game, menu = False, False
+            if event.key == pygame.K_ESCAPE: game, menu, end_scr = False, False, False
             elif event.key == pygame.K_RETURN: menu = False
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -47,12 +47,12 @@ while menu:
     if sleep_time>0: time.sleep(sleep_time)
 
     screen.blit(button_text, bt_rect[:2])
-    pygame.draw.rect(screen, white, pygame.Rect(b_rect), 2)
+    pygame.draw.rect(screen, white, b_rect, 2)
     screen.blit(title_text, tt_rect[:2])
     pygame.display.update()
 
 paddle_size = [5, 40]
-paddle_v = 3
+paddle_v = 5
 
 p1_pos = [50, (screen_size[1]-paddle_size[1])//2]
 p2_pos = [screen_size[0]-paddle_size[0]-50, (screen_size[1]-paddle_size[1])//2]
@@ -71,10 +71,10 @@ prev_time = time.time()
 while game:
     for event in pygame.event.get():
 
-        if event.type == pygame.QUIT: game = False
+        if event.type == pygame.QUIT: game, end_scr = False, False
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_ESCAPE: game = False
+            if event.key == pygame.K_ESCAPE: game, end_scr = False, False
 
     pressed_keys = pygame.key.get_pressed()
     if pressed_keys[pygame.K_w]:
@@ -110,11 +110,9 @@ while game:
         ball_v = [2., ball_v[1]*-1]
         hit_count=0
         score[1]+=1
-        if score[1]>10 and score[1]-score[0]>1: 
-            print('player 2 wins')
+        if score[1]>10 and score[1]-score[0]>1:
             game = False
         elif score[0]>17 and score[1]>17:
-            print('tie')
             game = False
 
     elif ball_pos[0]>screen_size[0]:
@@ -123,10 +121,8 @@ while game:
         hit_count=0
         score[0]+=1
         if score[0]>10 and score[0]-score[1]>1: 
-            print('player 1 wins')
             game = False
         elif score[0]>17 and score[1]>17:
-            print('tie')
             game = False
 
     elif ball_pos[1]-ball_r+ball_v[1] < 20 or ball_pos[1]+ball_r+ball_v[1] > screen_size[1]-20-2*ball_r: ball_v[1]*=-1
@@ -156,6 +152,40 @@ while game:
         pygame.draw.rect(screen, white, (net_line_x, net_line_y)+net_line_size)
         net_line_y+=net_line_size[1]+5
         
+    pygame.display.update()
+
+end_result_text = big_font.render('Tie!' if score[0] == score[1] else 'Player '+('1' if score[0]>score[1] else '2')+' wins!', False, white)
+end_result_text_rect = ((screen_size[0]-end_result_text.get_width())//2, screen_size[1]//3-end_result_text.get_height(), end_result_text.get_width(), end_result_text.get_height())
+
+exit_button_text = medium_font.render('Exit', False, white)
+exit_button_text_rect = ((screen_size[0]-exit_button_text.get_width())//2, screen_size[1]//2, exit_button_text.get_width(), exit_button_text.get_height())
+exit_button_rect = ((screen_size[0]-exit_button_text_rect[2]*1.5)//2, screen_size[1]//2-exit_button_text_rect[3]//3, exit_button_text_rect[2]*1.5, exit_button_text_rect[3]*1.5)
+
+prev_time = time.time()
+while end_scr:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            end_scr = False
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE: end_scr = False
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            if exit_button_rect[0] <= event.pos[0] and event.pos[0] <= exit_button_rect[0]+exit_button_rect[2]:
+                if exit_button_rect[1] <= event.pos[1] and event.pos[1] <= exit_button_rect[1]+exit_button_rect[3]: end_scr = False
+
+    current_time = time.time()
+    dt = current_time-prev_time
+    prev_time = current_time
+    sleep_time = 1./FPS-dt
+    if sleep_time>0: time.sleep(sleep_time)
+
+    screen.fill((0,0,0))
+    screen.blit(end_result_text, end_result_text_rect[:2])
+    screen.blit(exit_button_text, exit_button_text_rect[:2])
+    pygame.draw.rect(screen, white, exit_button_rect, 2)
+    
     pygame.display.update()
 
 pygame.quit()
